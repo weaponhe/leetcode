1b8b5dfb246aa9ad45e1dab196a8c1701a2f46a9
weaponhe <weaponhe@126.com>
2015-06-02 17:33:33 +0800

    add implement_trie

--- implement_trie.cpp
+++ implement_trie.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 
 class TrieNode {
 public:
+	char content;   // the character included
+	bool isend;     // if the node is the end of a word
+	vector<TrieNode*> children; // the children of the node
 	// Initialize your data structure here.
-	TrieNode() {
-		this->value = NULL;
+	TrieNode() :content(' '), isend(false) {}
+	TrieNode(char ch) :content(ch), isend(false) {}
+	TrieNode* subNode(char ch) {
+		if (!children.empty()) {
+			for (auto child : children) {
+				if (child->content == ch)
+					return child;
+			}
+		}
+		return nullptr;
 	}
-	TrieNode(char value) {
-		this->value = value;
+	~TrieNode() {
+		for (auto child : children)
+			delete child;
 	}
-private:
-	char value;
-	TrieNode* childs;
 };
 
 class Trie {
@@ -25,25 +33,47 @@ public:
 
 	// Inserts a word into the trie.
 	void insert(string s) {
-
+		if (search(s)) return;
+		TrieNode* curr = root;
+		for (auto ch : s) {
+			TrieNode* child = curr->subNode(ch);
+			if (child != nullptr) {
+				curr = child;
+			}
+			else {
+				TrieNode *newNode = new TrieNode(ch);
+				curr->children.push_back(newNode);
+				curr = newNode;
+			}
+		}
+		curr->isend = true;
 	}
 
 	// Returns if the word is in the trie.
 	bool search(string key) {
-
+		TrieNode* curr = root;
+		for (auto ch : key) {
+			curr = curr->subNode(ch);
+			if (curr == nullptr)
+				return false;
+		}
+		return curr->isend == true;
 	}
 
 	// Returns if there is any word in the trie
 	// that starts with the given prefix.
 	bool startsWith(string prefix) {
-
+		TrieNode* curr = root;
+		for (auto ch : prefix) {
+			curr = curr->subNode(ch);
+			if (curr == nullptr)
+				return false;
+		}
+		return true;
 	}
-
+	~Trie() {
+		delete root;
+	}
 private:
 	TrieNode* root;
 };
-
-// Your Trie object will be instantiated and called as such:
-// Trie trie;
-// trie.insert("somestring");
-// trie.search("key");
